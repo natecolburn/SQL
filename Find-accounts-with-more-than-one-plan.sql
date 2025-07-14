ed7e066a03132aee367383e6a577678c827cb352
Nathan Colburn <64048669+natecolburn@users.noreply.github.com>
2025-07-14 17:10:03 -0400

    Update Find-accounts-with-more-than-one-plan.sql

--- Find-accounts-with-more-than-one-plan.sql
+++ Find-accounts-with-more-than-one-plan.sql
@@ -5,4 +5,5 @@ SELECT
 FROM nyins.Accounts a
 JOIN nyins.Plans p ON a.AccountId = p.AccountId
 GROUP BY a.AccountId, a.AccountName
-HAVING COUNT(p.PlanId) > 1;
+HAVING COUNT(p.PlanId) > 1
+ORDER BY PlanCount DESC;
